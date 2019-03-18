8b1513d1515ad40c8a51ed5582bc7f8ee4134c57
Ahmed Abdulrahman <a.kasapbashi@live.com>
2019-03-18 10:28:45 +0100

    :wrench: Add BaseRules handler function

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-module.exports = require('./eslintrc.json');
+"use strict";
+
+const builtinRules = require("./rules/builtin");
+const reactRules = require("./rules/react");
+const importRules = require("./rules/import");
+
+function baseRules({
+  builtin = true,
+  import: import_ = false,
+  react = false,
+} = {}) {
+  return Object.assign(
+    builtin ? builtinRules : {},
+    import_ ? importRules : {},
+    react ? reactRules : {}
+  );
+}
+
+module.exports = baseRules;
